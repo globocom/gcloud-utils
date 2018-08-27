70736140dad5cb2eafe64e77b961d7fe857e43a2
Susana Bouchardet <susana.bouchardet@corp.globo.com>
2018-08-27 15:13:00 -0300

    change HttpMock to HttpMockSequence on test_storage

--- tests/test_storage.py
+++ tests/test_storage.py
@@ -1,16 +1,15 @@
 """Test Storage Module"""
 import unittest
-from googleapiclient.http import HttpMock
+from googleapiclient.http import HttpMockSequence
 from gcloud_utils.storage import Storage
 class TestStorage(unittest.TestCase):
     "Test Storage module"
 
     def test_get_abs_path(self):
         """Test get abs path"""
-        http_mocked = HttpMock('tests/mock/storage/first_result.json', {
-            'status': '200',
-            'content-type':'application/json'
-            })
+        http_mocked = HttpMockSequence([
+            ({'status': '200','content-type':'application/json'},open('tests/mock/storage/first_result.json', 'rb').read())
+            ])
 
         storage_test = Storage(bucket="teste", http=http_mocked)
         abs_path = storage_test.get_abs_path("path/something/inside/bucket")
