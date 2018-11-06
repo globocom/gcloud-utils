21f133bbc453459945aeb3b81949ce76a4b9211c
Gustavo Alves <gustavo.alves@corp.globo.com>
2018-11-06 10:40:14 -0200

    solving code smells

--- tests/test_ml_engine.py
+++ tests/test_ml_engine.py
@@ -12,7 +12,7 @@ class TestMlEngine(unittest.TestCase):
     def test_create_new_model(self):
         """Test request to create a new model"""
         http = HttpMock('tests/fixtures/ml_engine/first_result.json', {'status': '200'})
-        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION",http=http)
+        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
         post_to_create_model = ml_engine_test.create_new_model("MODEL", "DESCRIPTION")
 
         # Test Method
@@ -23,56 +23,71 @@ class TestMlEngine(unittest.TestCase):
         expected = """{"name": "MODEL", "description": "DESCRIPTION"}"""
         self.assertEqual(post_to_create_model.body, expected)
 
-    
     def test_create_new_model_version_4_5(self):
         """Test the creation of new model"""
         http = HttpMockSequence([
-            ({'status': '200'},open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
-            ({'status': '200'},open('tests/fixtures/ml_engine/versions_models.json', 'rb').read())
+            ({'status': '200'}, open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/versions_models.json', 'rb').read())
         
         ])
-        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION",http=http)
+        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
 
-        (post_to_create_model, new_version) = ml_engine_test.increase_model_version("MODEL","JOB_ID")
-        self.assertEqual(post_to_create_model.body,'{"deploymentUri": "gs://BUCKET_NAME/jobs/JOB_ID/export", "name": "v4_5"}')
+        (post_to_create_model, new_version) = ml_engine_test.increase_model_version("MODEL", "JOB_ID")
+
+        self.assertEqual(
+            post_to_create_model.body,
+            '{"deploymentUri": "gs://BUCKET_NAME/jobs/JOB_ID/export", "name": "v4_5"}'
+        )
         self.assertEqual(new_version, "v4_5")
 
     def test_create_new_model_version_5_0(self):
         """Test the creation of new model"""
         http = HttpMockSequence([
-            ({'status': '200'},open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
-            ({'status': '200'},open('tests/fixtures/ml_engine/versions_models_4_9.json', 'rb').read())
+            ({'status': '200'}, open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/versions_models_4_9.json', 'rb').read())
         
         ])
-        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION",http=http)
+        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
 
-        (post_to_create_model, new_version) = ml_engine_test.increase_model_version("MODEL","JOB_ID")
-        self.assertEqual(post_to_create_model.body,'{"deploymentUri": "gs://BUCKET_NAME/jobs/JOB_ID/export", "name": "v5_0"}')
+        (post_to_create_model, new_version) = ml_engine_test.increase_model_version("MODEL", "JOB_ID")
+
+        self.assertEqual(
+            post_to_create_model.body,
+            '{"deploymentUri": "gs://BUCKET_NAME/jobs/JOB_ID/export", "name": "v5_0"}'
+        )
         self.assertEqual(new_version,"v5_0")
+
     def test_create_new_model_version_empty(self):
         """Test the creation of new model"""
         http = HttpMockSequence([
-            ({'status': '200'},open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
-            ({'status': '200'},open('tests/fixtures/ml_engine/versions_models_empty.json', 'rb').read())
+            ({'status': '200'}, open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/versions_models_empty.json', 'rb').read())
         
         ])
-        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION",http=http)
+        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
 
-        (post_to_create_model, new_version) = ml_engine_test.increase_model_version("MODEL","JOB_ID")
-        self.assertEqual(post_to_create_model.body,'{"deploymentUri": "gs://BUCKET_NAME/jobs/JOB_ID/export", "name": "v0_1"}')
+        (post_to_create_model, new_version) = ml_engine_test.increase_model_version("MODEL", "JOB_ID")
+
+        self.assertEqual(
+            post_to_create_model.body,
+            '{"deploymentUri": "gs://BUCKET_NAME/jobs/JOB_ID/export", "name": "v0_1"}'
+        )
         self.assertEqual(new_version,"v0_1")
 
     def test_set_model_version_as_default(self):
         """"Test method that set version as default"""
         http = HttpMockSequence([
-            ({'status': '200'},open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
-            ({'status': '200'},open('tests/fixtures/ml_engine/versions_models_empty.json', 'rb').read())
+            ({'status': '200'}, open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/versions_models_empty.json', 'rb').read())
         ])
-        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION",http=http)
-        result = ml_engine_test.set_version_as_default("model_teste","v1_0")
+        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
+        result = ml_engine_test.set_version_as_default("model_teste", "v1_0")
 
         self.assertEqual(result.body, '{}')
-        self.assertEqual(result.uri, "https://ml.googleapis.com/v1/projects/PROJECT/models/model_teste/versions/v1_0:setDefault?alt=json")
+        self.assertEqual(
+            result.uri,
+            "https://ml.googleapis.com/v1/projects/PROJECT/models/model_teste/versions/v1_0:setDefault?alt=json"
+        )
 
     @freeze_time("1994-04-27 12:00:01")
     def test_start_prediciton_job(self):
@@ -82,7 +97,7 @@ class TestMlEngine(unittest.TestCase):
         input_file = "gs://BUCKET/recommendation/matrix_prefs/PRODUTO/input/part-00000"
         output_file="gs://BUCKET/recommendation/matrix_prefs/PRODUTO/output/part-00000"
 
-        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION",http=http)
+        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
 
         post_to_create = ml_engine_test.start_predict_job("PRODUTO", "MODEL_NAME", [input_file], output_file)
 
@@ -103,34 +118,34 @@ class TestMlEngine(unittest.TestCase):
         http = HttpMock('tests/fixtures/ml_engine/first_result.json', {'status': '200'})
 
         input_file = "gs://BUCKET/recommendation/matrix_prefs/PRODUTO/input/part-00000"
-        output_file="gs://BUCKET/recommendation/matrix_prefs/PRODUTO/output/part-00000"
+        output_file = "gs://BUCKET/recommendation/matrix_prefs/PRODUTO/output/part-00000"
 
-        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION",http=http)
+        ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
 
-        self.assertRaises(TypeError ,ml_engine_test.start_predict_job, "PRODUTO", "MODEL_NAME", input_file, output_file)
+        self.assertRaises(TypeError, ml_engine_test.start_predict_job, "PRODUTO", "MODEL_NAME", input_file, output_file)
 
     def test_job_list(self):
         """TEST JOB LIST AND FILTER BY FINAL STATE"""
         jobs_mock = open('tests/fixtures/ml_engine/jobs.json', 'rb').read()
 
         http = HttpMockSequence([
-            ({'status': '200'},open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
-            ({'status': '200'},jobs_mock),
-            ({'status': '200'},jobs_mock),
-            ({'status': '200'},jobs_mock)
+            ({'status': '200'}, open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
+            ({'status': '200'}, jobs_mock),
+            ({'status': '200'}, jobs_mock),
+            ({'status': '200'}, jobs_mock)
         ])
 
         ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
-        self.assertEqual(len(ml_engine_test.list_jobs()),19)
-        self.assertEqual(len(ml_engine_test.list_jobs(filter_final_state=None)),20)
-        self.assertEqual(len(ml_engine_test.list_jobs(filter_final_state="STATE_WRONG")),0)
+        self.assertEqual(len(ml_engine_test.list_jobs()), 19)
+        self.assertEqual(len(ml_engine_test.list_jobs(filter_final_state=None)), 20)
+        self.assertEqual(len(ml_engine_test.list_jobs(filter_final_state="STATE_WRONG")), 0)
 
     def test_get_job(self):
         """Test getting a job"""
 
         http = HttpMockSequence([
-            ({'status': '200'},open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
-            ({'status': '200'},open('tests/fixtures/ml_engine/get_job_succeeded.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/get_job_succeeded.json', 'rb').read()),
         ])
 
         job_id = "globoplay_autoencoder_keras_2018_08_06_10_00_37"
@@ -143,9 +158,9 @@ class TestMlEngine(unittest.TestCase):
         """Test waiting job to finish execution"""
 
         http = HttpMockSequence([
-            ({'status': '200'},open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
-            ({'status': '200'},open('tests/fixtures/ml_engine/get_job_running.json', 'rb').read()),
-            ({'status': '200'},open('tests/fixtures/ml_engine/get_job_succeeded.json', 'rb').read())
+            ({'status': '200'}, open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/get_job_running.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/get_job_succeeded.json', 'rb').read())
         ])
 
         job_id = "globoplay_autoencoder_keras_2018_08_06_10_00_37"
@@ -177,8 +192,8 @@ class TestMlEngine(unittest.TestCase):
         """
 
         http = HttpMockSequence([
-            ({'status': '200'},open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
-            ({'status': '200'},open('tests/fixtures/ml_engine/versions_models.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/first_result.json', 'rb').read()),
+            ({'status': '200'}, open('tests/fixtures/ml_engine/versions_models.json', 'rb').read()),
         ])
 
         ml_engine_test = ml_engine.MlEngine("PROJECT", "BUCKET_NAME", "REGION", http=http)
