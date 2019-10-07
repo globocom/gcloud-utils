4bd4528bf420253646f7cc58f10f9d7e1da50d81
Héricles Emanuel <hericles@lsd.ufcg.edu.br>
2019-10-07 16:05:10 -0300

    function execution implementation

--- gcloud_utils/functions.py
+++ gcloud_utils/functions.py
@@ -9,25 +9,22 @@ logging.basicConfig(level=logging.DEBUG,
 
 class Functions(object):
     """Google-Cloud-Functions handler"""
-    
+
     def __init__(self, project, zone):
         self.project = project
         self.zone = zone
         self.logger = logging.getLogger(name=self.__class__.__name__)
         self.client = discovery.build('cloudfunctions', 'v1')
-    
 
     def __get_functions_resource(self, client):
-        return client.projects()
-                    .locations()
-                    .functions()
+        return client.projects().locations().functions()
 
-
-    def create_function(self, name, location, body):
+    def create_function(self, name, body):
         """Create a Cloud Function"""
+        location = "projects/{}/locations/{}".format(self.project, self.zone)
         self.__get_functions_resource(self.client).create(location, body)
-    
 
-    def call_function(self, name, body):
+    def call_function(self, name, data):
         """Call a Cloud Function"""
-        self.__get_functions_resource(self.client).call(name, body)
+        function = "projects/{}/locations/{}/functions/{}".format(self.project, self.zone, name)
+        self.__get_functions_resource(self.client).call(function, {"data": data})
