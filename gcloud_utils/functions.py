3f9d1751ab8812c0a88c6b9653ba2fb058080480
Héricles Emanuel <hericles.me@gmail.com>
2019-10-07 23:47:44 -0300

    request execution

--- gcloud_utils/functions.py
+++ gcloud_utils/functions.py
@@ -15,16 +15,26 @@ class Functions(object):
         self.zone = zone
         self.logger = logging.getLogger(name=self.__class__.__name__)
         self.client = discovery.build('cloudfunctions', 'v1')
+        self.functions = self.__get_functions_resource(self.client)
 
     def __get_functions_resource(self, client):
         return client.projects().locations().functions()
 
+    def __execute_request(self, request):
+        return request.execute()
+
     def create_function(self, name, body):
         """Create a Cloud Function"""
         location = "projects/{}/locations/{}".format(self.project, self.zone)
-        self.__get_functions_resource(self.client).create(location, body)
+        request = self.functions.create(location=location)
+        return self.__execute_request(request)
 
     def call_function(self, name, data):
         """Call a Cloud Function"""
-        function = "projects/{}/locations/{}/functions/{}".format(self.project, self.zone, name)
-        self.__get_functions_resource(self.client).call(function, {"data": data})
+        function = "projects/{}/locations/{}/functions/{}".format(
+            self.project, self.zone, name)
+
+        body = {"data": data}
+        request = self.functions.call(name=function, body=body)
+
+        return self.__execute_request(request)
