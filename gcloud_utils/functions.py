dfc078195595658e1e46d8a3bfa237f94da67da0
Héricles Emanuel <hericles@lsd.ufcg.edu.br>
2019-10-14 17:58:11 -0300

    function build

--- gcloud_utils/functions.py
+++ gcloud_utils/functions.py
@@ -1,5 +1,6 @@
 """Module to handle Google Cloud Functions Service"""
 
+import os
 import logging
 from googleapiclient import discovery
 
@@ -20,14 +21,19 @@ class Functions(object):
     def __get_functions_resource(self, client):
         return client.projects().locations().functions()
 
+    def __build_function(self, path):
+        pass
+
+
     def __execute_request(self, request):
         return request.execute()
 
-    def create_function(self, name, body):
+    def create_function(self, name, runtime, path=os.getcwd()):
         """Create a Cloud Function"""
-        location = "projects/{}/locations/{}".format(self.project, self.zone)
-        request = self.functions.create(location=location)
-        return self.__execute_request(request)
+        # location = "projects/{}/locations/{}".format(self.project, self.zone)
+        # request = self.functions.create(location=location)
+        # return self.__execute_request(request)
+        return("runtime: " + runtime + " path: " + path)
 
     def call_function(self, name, data):
         """Call a Cloud Function"""
---
+++ gcloud_utils/test.py
@@ -0,0 +1,4 @@
+from functions import Functions
+
+f = Functions("inspire-1564763247029", "us-central1")
+print(f.create_function("oie", "python3"))
