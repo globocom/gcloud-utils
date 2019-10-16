0e4ec10de0e11ab7ee2f2c7e0c52eb08d3dfb776
Héricles Emanuel <hericles@lsd.ufcg.edu.br>
2019-10-16 18:16:15 -0300

    add create function + conflict verification

--- gcloud_utils/functions.py
+++ gcloud_utils/functions.py
@@ -1,8 +1,10 @@
 """Module to handle Google Cloud Functions Service"""
 
 import os
+import json
 import logging
 from googleapiclient import discovery
+from googleapiclient.errors import HttpError
 
 logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s - %(levelname)s - %(name)s - %(message)s')
@@ -15,32 +17,59 @@ class Functions(object):
         self.project = project
         self.zone = zone
         self.logger = logging.getLogger(name=self.__class__.__name__)
-        self.client = discovery.build('cloudfunctions', 'v1')
+        self.client = discovery.build('cloudfunctions', 'v1beta2')
         self.functions = self.__get_functions_resource(self.client)
+        self.parent = self.__build_parent()
+
+    def __check_runtime(self):
+        pass
+
+    def __build_parent(self):
+        return "projects/{}/locations/{}".format(self.project, self.zone)
 
     def __get_functions_resource(self, client):
         return client.projects().locations().functions()
 
-    def __build_function(self, path):
-        pass
+    def __get_upload_url(self):
+        generate_upload_url_request = self.functions.generateUploadUrl(
+            parent=self.parent)
+        res = self.__execute_request(generate_upload_url_request)
+        return res['uploadUrl']
 
+    def __build_function(self, name, runtime, path,  trigger):
+        upload_url = self.__get_upload_url()
+        body = {
+            "entryPoint": name,
+            "runtime": runtime,
+            "sourceUploadUrl": upload_url,
+            "name": self.parent + '/functions/' + name,
+        }
+
+        return self.functions.create(location=self.parent, body=body)
 
     def __execute_request(self, request):
         return request.execute()
 
-    def create_function(self, name, runtime, path=os.getcwd()):
-        """Create a Cloud Function"""
-        # location = "projects/{}/locations/{}".format(self.project, self.zone)
-        # request = self.functions.create(location=location)
-        # return self.__execute_request(request)
-        return("runtime: " + runtime + " path: " + path)
+    def create_function(self, name, runtime, trigger, path=os.getcwd()):
+        function = self.__build_function(name, runtime, path, trigger)
+
+        try:
+            res = self.__execute_request(function)
+        except HttpError as err:
+            header, body = err.args
+            err_message = json.loads(body.decode('utf-8'))['error']['message']
+            print('[ERROR] ' + err_message)
+        return res
+
+    def list_functions(self):
+        pass
 
     def call_function(self, name, data):
         """Call a Cloud Function"""
+
         function = "projects/{}/locations/{}/functions/{}".format(
             self.project, self.zone, name)
 
-        body = {"data": data}
-        request = self.functions.call(name=function, body=body)
+        request = self.functions.call(name=function, body={"data": data})
 
         return self.__execute_request(request)
