817ad358141eaf355121a545df457fa1ab5eebd0
Héricles Emanuel <hericles.me@gmail.com>
2019-10-17 00:02:11 -0300

    update docstrings

--- gcloud_utils/functions.py
+++ gcloud_utils/functions.py
@@ -20,14 +20,14 @@ class Functions(object):
         self.zone = zone
         self.logger = logging.getLogger(name=self.__class__.__name__)
         self.client = discovery.build('cloudfunctions', 'v1beta2')
-        self.functions = self.__get_functions_resource(self.client)
+        self.functions = self.__get_functions_resource()
         self.parent = self.__build_parent()
 
     def __build_parent(self):
         return 'projects/{}/locations/{}'.format(self.project, self.zone)
 
-    def __get_functions_resource(self, client):
-        return client.projects().locations().functions()
+    def __get_functions_resource(self):
+        return self.client.projects().locations().functions()
 
     def __get_upload_url(self):
         generate_upload_url_request = self.functions.generateUploadUrl(
@@ -38,7 +38,7 @@ class Functions(object):
     def __execute_request(self, request):
         return request.execute()
 
-    def __upload_function(path, filename, upload_url):
+    def __upload_function(self, path, filename, upload_url):
         name, extension = os.path.splitext(filename)
         self.__compress_function(path, name, extension)
 
@@ -75,6 +75,7 @@ class Functions(object):
         zip.close()
 
     def create_function(self, name, runtime, trigger, path=os.getcwd()):
+        """Create and Deploy a Cloud Function"""
         request = self.__build_function(name, runtime, path, trigger)
 
         try:
@@ -86,10 +87,12 @@ class Functions(object):
         return res
 
     def list_functions(self):
+        """List the cloud functions"""
         request = self.functions.list(location=self.parent)
         return self.__execute_request(request)
 
     def describe_function(self, name):
+        """Describe a function"""
         request = self.functions.get(
             name='{}/functions/{}'.format(self.parent, name))
         return self.__execute_request(request)
