42cf6365a946167a0bda18cce3ad02d20428e157
Gabriel Moura <gab.mv@hotmail.com>
2020-10-31 22:46:43 -0300

    add list_functions_test

---
+++ tests/test_functions.py
@@ -0,0 +1,22 @@
+"""Test functions.py"""
+import unittest
+from unittest.mock import MagicMock
+
+from gcloud_utils import functions
+
+PROJECT = "teste1"
+ZONE = "teste2"
+
+
+class TestFunctions(unittest.TestCase):
+    def setUp(self):
+        self.functions = functions.Functions(PROJECT, ZONE)
+
+    def test_list_functions(self):
+        self.functions = MagicMock()
+        self.functions__execute_request = MagicMock()
+
+        self.functions.list_functions()
+
+        self.functions.assert_called_once()
+        self.functions__execute_request.asser_called_once()
